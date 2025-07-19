9851ec71e373d6ccf1386d81517fbbdedb976cc2
Ivan Semikin <i.semikin.94@icloud.com>
2025-07-19 13:49:53 +0200

    Initial Commit

---
+++ MemePrediction/MemePredictionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemePredictionApp.swift
+//  MemePrediction
+//
+//  Created by Иван Семикин on 19/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct MemePredictionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
